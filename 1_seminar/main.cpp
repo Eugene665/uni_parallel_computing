6b09563171b270da3c91485c7ecac30313780e24
Eugenii R <it669@mail.ru>
2024-09-10 01:41:25 +0300

    Work version with BubbleSort, ShakerSort, SelectionSort, fixed a bug in displaying the sorted shaker list

--- 1_seminar/main.cpp
+++ 1_seminar/main.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 // Функция для обмена значений без затирки
-void Swap(int& a, int& b){
+void Swap(int& a, int& b) {
   int temp = a;
   a = b;
   b = temp;
 }
-
 
 class Sort {
 public:
@@ -23,7 +22,7 @@ public:
     for (int i = 0; i < length_arr - 1; i++){
       // Проходим по списку до отсортированной части, 
       // до чисел, которые уже "всплыли"
-      for (int j = 0; j < length_arr - i - 1; j++){
+      for (int j = 0; j < length_arr - i - 1; j++) {
         // Если впереди стоящее число меньше,
         // числа меняюся местами
         if (arr[j] > arr[j + 1]) {
@@ -37,28 +36,44 @@ public:
 
 class ShakerSort : public Sort{
   public:
-    void sort(int* arr, int length_arr) override{
+    void sort(int* arr, int length_arr) override {
       // Вводим переменные позиций на левый и правый 
       // участок неотсортированного массив
-      int left_pose = 1;
+      int left_pose = 0;
       int right_pose = length_arr - 1;
-      while (left_pose <= right_pose){
-        for (int i = right_pose; i >= left_pose; i--){
-          if (arr[i - 1] > arr[i]){
-              Swap(arr[i], arr[i + 1]);
+      while (left_pose <= right_pose) {
+        for (int i = right_pose; i > left_pose; i--) {
+          if (arr[i - 1] > arr[i]) {
+              Swap(arr[i - 1], arr[i]);
           }
-          left_pose++;
         }
-        for (int i = left_pose; i <= right_pose; i++){
-          if (arr[i - 1] > arr[i]){
+        left_pose++;
+        for (int i = left_pose; i < right_pose; i++) {
+          if (arr[i] > arr[i + 1]){
             Swap(arr[i], arr[i + 1]);
           }
-          right_pose--;
         }
+        right_pose--;
       }
     }
 };
 
+class SelectionSort : public Sort{
+  public:
+    void sort(int* arr, int length_arr) override {
+      for (int i = 0; i < length_arr - 1; i++) {
+        int smallest_pose = i;
+        for (int j = i + 1; j < length_arr; j++) {
+          if (arr[j] < arr[smallest_pose]) {
+            smallest_pose = j;
+          }
+        }
+        if (smallest_pose != i) {
+          Swap(arr[i], arr[smallest_pose]);
+        }
+      }
+    }
+};
 
 int main() {
 
@@ -84,7 +99,7 @@ int main() {
 
     // Вывод исходного массива
     cout << "Исходный массив\n";
-    for (int i = 0; i < length_arr; i++){
+    for (int i = 0; i < length_arr; i++) {
       cout << arr[i] << " ";
     }
     cout << "\n\n";
@@ -92,7 +107,7 @@ int main() {
     //BubbleSort---------------------------------------
     // Копирования массива для сортировки пузырьком
     int bubble_arr[length_arr];
-    for (int i = 0; i < length_arr; i++){
+    for (int i = 0; i < length_arr; i++) {
       bubble_arr[i] = arr[i];
     }
 
@@ -116,7 +131,7 @@ int main() {
     //ShakerSort-------------------------------------------
     // Копирования массива для сортировки шейкером
     int shaker_arr[length_arr];
-    for (int i = 0; i < length_arr; i++){
+    for (int i = 0; i < length_arr; i++) {
       shaker_arr[i] = arr[i];
     }
 
@@ -131,11 +146,36 @@ int main() {
     // Вызов метода сортировки
     sorter_shaker.sort(shaker_arr, length_arr);
 
-    // Вывод отсортированного вектора
+    // Вывод отсортированного массив
     cout << "После сортировки шейкером\n";
     for (int i = 0; i < length_arr; i++)
-        cout << bubble_arr[i] << " ";
+        cout << shaker_arr[i] << " ";
     cout << "\n\n";
+
+    //SelectionSort----------------------------------------
+    // Копирования массива для сортировки выбором
+    int  selection_arr[length_arr];
+    for (int i = 0; i < length_arr; i++) {
+      selection_arr[i] = arr[i];
+    }
+
+    // Вывод массива до сортировки
+    cout << "До сортировки выбором\n";
+    for (int i = 0; i < length_arr; i++)
+        cout << selection_arr[i] << " ";
+    cout << "\n";
+
+    // Создание объекта SelectionSort
+    SelectionSort sorter_selection;
+    // Вызов метода сортировки
+    sorter_selection.sort(selection_arr, length_arr);
+
+    // Вывод отсортированного массива
+    cout << "После сортировки выбором\n";
+    for (int i = 0; i < length_arr; i++)
+        cout << selection_arr[i] << " ";
+    cout << "\n\n"; 
+
 
     return 0;
 }
