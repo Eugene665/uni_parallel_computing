95cc47bfe74510798d7d162343620e9af5652c4e
Evgenii R <it669@mail.ru>
2024-09-09 21:08:15 +0300

    WIP: added bubble sort(not work)

--- 1_seminar/main.cpp
+++ 1_seminar/main.cpp
@@ -1,4 +1,4 @@
-
+#include <vector>
 #include <iostream>
 using namespace std;
 
@@ -18,20 +18,81 @@ class BubbleSort : public Sort {
 public:
   // Переопределение функции
     void sort(vector<int>& arr) override {
-            
+        int size = arr.size();
+        // Проходим по всему списку
+        for (int i = 0; i < size - 1; i++){
+          // Проходим по списку до отсортированной части, 
+          // до чисел, которые уже "всплыли"
+          for (int j = 0; j < size - i - 1; j++){
+            // Если впереди стоящее число меньше,
+            // числа меняюся местами
+            if (arr[j] > arr[j + 1]) {
+              Swap(arr[j], arr[j + 1]);
             }
+          }
+        }
+      }
+};
+
+
+class ShakerSort : public Sort{
+  public:
+    void sort(vector<int>& arr) override{
+      int size = arr.size();
+      int left_pose = 1;
+      int right_pose = size - 1;
+      while (left_pose <= right_pose){
+        for (int i = right_pose; i >= left_pose; i--){
+          if (arr[i - 1] > arr[i]){
+              Swap(arr[i], arr[i + 1]);
+          }
+          left_pose++;
+        }
+        for (int i = left_pose; i <= right_pose; i++){
+          if (arr[i - 1] > arr[i]){
+            Swap(arr[i], arr[i + 1]);
+          }
+          right_pose--;
+        }
+      }
+    }
 };
 
 int main() {
-    vector<int> vec = {2, 3, 1, 4, 5};  // Создание вектора
 
-    BubbleSort sorter;  // Создание объекта BubbleSort
-    sorter.sort(vec);   // Вызов метода сортировки
+    setlocale(LC_ALL,"Rus");
+
+    vector<int> vec = {6, 10, 1, 3, 2};  // Создание вектора
+    // Вывод вектора о сортировки
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << "\n";
+
+    BubbleSort sorter_bubble;  // Создание объекта BubbleSort
+    sorter_bubble.sort(vec);   // Вызов метода сортировки
+    
+    cout << "Сортировка пузырьком\n";
+    // Вывод отсортированного вектора
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << "\n";
+
+    //ShakerSort-------------------------------------------
+    vec = {6, 10, 1, 3, 2};  // Создание вектора
+
+    // Вывод вектора о сортировки
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << "\n";
+
+    ShakerSort sorter_shaker;  // Создание объекта BubbleSort
+    sorter_shaker.sort(vec);   // Вызов метода сортировки
 
     // Вывод отсортированного вектора
     for (int i = 0; i < vec.size(); i++)
         cout << vec[i] << " ";
-    cout << "\n"
+    cout << "\n";
+
     return 0;
 }
 
